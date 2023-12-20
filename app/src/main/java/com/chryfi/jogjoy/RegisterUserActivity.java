a0bbe165c0bfdde1eec00a9b76b19f37e0e3087a
Chryfi <71967555+Chryfi@users.noreply.github.com>
2023-12-20 15:08:52 +0100

    Add rounding and event listeners to register user

--- app/src/main/java/com/chryfi/jogjoy/RegisterUserActivity.java
+++ app/src/main/java/com/chryfi/jogjoy/RegisterUserActivity.java
@@ -2,11 +2,19 @@ package com.chryfi.jogjoy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.SQLException;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.chryfi.jogjoy.data.User;
 import com.chryfi.jogjoy.data.tables.UserTable;
@@ -18,6 +26,22 @@ public class RegisterUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        EditText weight = this.findViewById(R.id.weight_input);
+        EditText height = this.findViewById(R.id.height_input);
+
+        /* event listeners when the user finished typing the text to auto correct it */
+        weight.setOnFocusChangeListener((view, hasFocus) -> {
+            if (!hasFocus) {
+                weight.setText(String.valueOf(roundWeight(Float.parseFloat(weight.getText().toString()))));
+            }
+        });
+
+        height.setOnFocusChangeListener((view, hasFocus) -> {
+            if (!hasFocus) {
+                height.setText(String.valueOf(roundHeight(Float.parseFloat(height.getText().toString()))));
+            }
+        });
     }
 
     public void registerUser(View view) {
@@ -28,6 +52,7 @@ public class RegisterUserActivity extends AppCompatActivity {
         EditText weight = this.findViewById(R.id.weight_input);
         EditText height = this.findViewById(R.id.height_input);
 
+        /* check if everything has been input */
         if (username.getText().toString().isEmpty()
                 || password0.getText().toString().isEmpty() || password1.getText().toString().isEmpty()
                 || weight.getText().toString().isEmpty() || height.getText().toString().isEmpty()
@@ -37,6 +62,7 @@ public class RegisterUserActivity extends AppCompatActivity {
 
         boolean error = false;
 
+        /* check if username already exists */
         try (UserTable userTable = new UserTable(this)) {
             if (userTable.getUserByUsername(username.getText().toString()).isPresent()) {
                 username.setError(this.getResources().getString(R.string.username_already_exists));
@@ -44,8 +70,13 @@ public class RegisterUserActivity extends AppCompatActivity {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+            new AlertDialog.Builder(this)
+                    .setMessage(this.getResources().getString(R.string.database_error))
+                    .show();
+            return;
         }
 
+        /* check if passwords match */
         if (!password0.getText().toString().equals(password1.getText().toString())) {
             password0.getText().clear();
             password1.getText().clear();
@@ -54,6 +85,7 @@ public class RegisterUserActivity extends AppCompatActivity {
             error = true;
         }
 
+        /* validate number ranges */
         if (Float.parseFloat(weight.getText().toString()) <= 0
                 || Float.parseFloat(weight.getText().toString()) >= 1000) {
             weight.setError(this.getResources().getString(R.string.number_out_range));
@@ -83,15 +115,42 @@ public class RegisterUserActivity extends AppCompatActivity {
 
         String password = password0.getText().toString();
         String usernameValue = username.getText().toString();
-        float weightValue = Math.round(Float.parseFloat(weight.getText().toString()) * 100F) / 100F;
-        int heightValue = Math.round(Float.parseFloat(height.getText().toString()));
+        float weightValue = this.roundWeight(Float.parseFloat(weight.getText().toString()));
+        int heightValue = this.roundHeight(Float.parseFloat(height.getText().toString()));
 
         User user = new User(usernameValue, password, weightValue, heightValue, genderValue);
 
         try (UserTable userTable = new UserTable(this)) {
-            userTable.insertUser(user);
+            if (userTable.insertUser(user)) {
+                this.startActivity(new Intent(this, LoginUserActivity.class));
+            } else {
+                new AlertDialog.Builder(this)
+                        .setMessage(this.getResources().getString(R.string.database_error))
+                        .show();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
+            new AlertDialog.Builder(this)
+                    .setMessage(this.getResources().getString(R.string.database_error))
+                    .show();
         }
+    }
+
+    /**
+     * Guarantee that event listeners and other methods round the same.
+     * @param rawWeight
+     * @return rounded weight
+     */
+    private float roundWeight(float rawWeight) {
+        return Math.round(rawWeight * 100F) / 100F;
+    }
+
+    /**
+     * Guarantee that event listeners and other methods round the same.
+     * @param rawHeight
+     * @return rounded height.
+     */
+    private int roundHeight(float rawHeight) {
+        return Math.round(rawHeight);
     }
 }

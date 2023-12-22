8f16930bca9e8af266988c611fdd553f8d37fd76
Chryfi <71967555+Chryfi@users.noreply.github.com>
2023-12-22 12:10:54 +0100

    Add startRun and logout methods

--- app/src/main/java/com/chryfi/jogjoy/RunStartActivity.java
+++ app/src/main/java/com/chryfi/jogjoy/RunStartActivity.java
@@ -1,6 +1,8 @@
 package com.chryfi.jogjoy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,15 @@ public class RunStartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_run_userdata);
+    }
+
+    public void logout(View view) {
+        MainActivity.logoutUser();
+        this.startActivity(new Intent(this, MainActivity.class));
+        this.finish();
+    }
+
+    public void startRun(View view) {
+        this.startActivity(new Intent(this, RunActivity.class));
     }
 }

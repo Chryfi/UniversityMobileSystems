8666516bab22f98b2aa92d47d3752f9ad9da3a8d
Chryfi <71967555+Chryfi@users.noreply.github.com>
2023-12-22 13:20:47 +0100

    Add run start activity functionality

--- app/src/main/java/com/chryfi/jogjoy/RunStartActivity.java
+++ app/src/main/java/com/chryfi/jogjoy/RunStartActivity.java
@@ -3,14 +3,28 @@ package com.chryfi.jogjoy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RunStartActivity extends AppCompatActivity {
+    public final static String RUNGOAL_MESSAGE = "JobJoy.RUNGOALDISTANCE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_run_userdata);
+
+        EditText runGoal = this.findViewById(R.id.run_goal);
+
+        /* event listeners when the user finished typing the text to auto correct it */
+        runGoal.setOnFocusChangeListener((view, hasFocus) -> {
+            if (!hasFocus) {
+                try {
+                    runGoal.setText(String.valueOf(this.roundRunGoal(Float.parseFloat(runGoal.getText().toString()))));
+                } catch (NumberFormatException e) { }
+            }
+        });
     }
 
     public void logout(View view) {
@@ -20,6 +34,45 @@ public class RunStartActivity extends AppCompatActivity {
     }
 
     public void startRun(View view) {
-        this.startActivity(new Intent(this, RunActivity.class));
+        EditText runGoal = this.findViewById(R.id.run_goal);
+
+        if (!this.validateInput()) return;
+        float runGoalValue = Math.round(Float.parseFloat(runGoal.getText().toString()) * 100) / 100F;
+
+        Intent intent = new Intent(this, RunActivity.class);
+        intent.putExtra(RUNGOAL_MESSAGE, runGoalValue);
+        this.startActivity(intent);
+    }
+
+    /**
+     * Validates inputs and sets error messages to the UI.
+     * @return false if there has been an error.
+     */
+    private boolean validateInput() {
+        EditText runGoal = this.findViewById(R.id.run_goal);
+
+        if (runGoal.getText().toString().isEmpty()) {
+            runGoal.setError(this.getResources().getString(R.string.input_missing));
+            return false;
+        }
+
+        try {
+            Float.parseFloat(runGoal.getText().toString());
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        float runGoalValue = this.roundRunGoal(Float.parseFloat(runGoal.getText().toString()));
+
+        if (runGoalValue == 0.0F) {
+            runGoal.setError(this.getResources().getString(R.string.run_goal_number_low));
+            return false;
+        }
+
+        return true;
+    }
+
+    private float roundRunGoal(float runGoal) {
+        return Math.round(runGoal * 10) / 10F;
     }
 }

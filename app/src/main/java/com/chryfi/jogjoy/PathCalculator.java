b30d7d69b7277aa4370a3131d6893f84baf01223
Chryfi <71967555+Chryfi@users.noreply.github.com>
2023-12-27 23:10:34 +0100

    Add Path Calculator

---
+++ app/src/main/java/com/chryfi/jogjoy/PathCalculator.java
@@ -0,0 +1,37 @@
+package com.chryfi.jogjoy;
+
+import com.chryfi.jogjoy.data.GPSPoint;
+
+import java.util.Collection;
+import java.util.List;
+
+public class PathCalculator {
+    private static final double EARTH_RADIUS = 6371000;
+
+    public static double haversine(double lat1, double lon1, double lat2, double lon2) {
+        lat1 = Math.toRadians(lat1);
+        lon1 = Math.toRadians(lon1);
+        lat2 = Math.toRadians(lat2);
+        lon2 = Math.toRadians(lon2);
+
+        double dlat = lat2 - lat1;
+        double dlon = lon2 - lon1;
+        double a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return EARTH_RADIUS * c;
+    }
+
+    public static double calculatePathLength(List<GPSPoint> coordinates) {
+        double totalLength = 0;
+
+        for (int i = 0; i < coordinates.size() - 1; i++) {
+            GPSPoint p0 = coordinates.get(i);
+            GPSPoint p1 = coordinates.get(i + 1);
+            double distance = haversine(p0.getLatitude(), p0.getLongitude(), p1.getLatitude(), p1.getLongitude());
+            totalLength += distance;
+        }
+
+        return totalLength;
+    }
+}

23223bb7942a8cbe9005c2a4b1303541655a525b
Chryfi <71967555+Chryfi@users.noreply.github.com>
2024-01-07 18:12:14 +0100

    Add run list to runStart Activity

--- app/src/main/java/com/chryfi/jogjoy/RunStartActivity.java
+++ app/src/main/java/com/chryfi/jogjoy/RunStartActivity.java
@@ -1,11 +1,25 @@
 package com.chryfi.jogjoy;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.database.SQLException;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.chryfi.jogjoy.data.GPSPoint;
+import com.chryfi.jogjoy.data.Run;
+import com.chryfi.jogjoy.data.tables.RunTable;
+
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class RunStartActivity extends AppCompatActivity {
     public final static String RUNGOAL_MESSAGE = "JogJoy.RUNGOALDISTANCE";
@@ -17,7 +31,7 @@ public class RunStartActivity extends AppCompatActivity {
 
         EditText runGoal = this.findViewById(R.id.run_goal);
 
-        /* event listeners when the user finished typing the text to auto correct it */
+        /* event listeners when the user finished typing the text to round it */
         runGoal.setOnFocusChangeListener((view, hasFocus) -> {
             if (!hasFocus) {
                 try {
@@ -25,6 +39,37 @@ public class RunStartActivity extends AppCompatActivity {
                 } catch (NumberFormatException e) { }
             }
         });
+
+        this.listRuns();
+    }
+
+    private void listRuns() {
+        try (RunTable runTable = new RunTable(this)) {
+            List<Run> runs = runTable.getRunsDescTime(MainActivity.getLoggedinUsername());
+
+            for (Run run : runs) {
+                List<GPSPoint> points = run.getGpspoints();
+
+                TextView runText = new TextView(this);
+                runText.setBackground(this.getDrawable(R.drawable.border));
+                String runElement = this.getResources().getString(R.string.run_element);
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm dd.MM.yyyy", Locale.getDefault());
+                SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+                DecimalFormat format = new DecimalFormat("0.##");
+                long time = (points.get(points.size() - 1).getTimestamp() - points.get(0).getTimestamp());
+                double path = PathCalculator.calculatePathLength(run.getGpspoints()) / 1000D;
+
+                runText.setText(String.format(runElement,
+                        dateFormat.format(points.get(0).getTimestamp()), timeFormat.format(time),
+                        format.format(path), format.format(run.getGoal())));
+
+                LinearLayout scrollView = this.findViewById(R.id.runs_scroll_linear);
+                scrollView.addView(runText);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void logout(View view) {
@@ -41,6 +86,7 @@ public class RunStartActivity extends AppCompatActivity {
 
         if (!this.validateInput()) return;
         float runGoalValue = Math.round(Float.parseFloat(runGoal.getText().toString()) * 100) / 100F;
+        runGoal.setText("");
 
         Intent intent = new Intent(this, RunActivity.class);
         intent.putExtra(RUNGOAL_MESSAGE, runGoalValue);

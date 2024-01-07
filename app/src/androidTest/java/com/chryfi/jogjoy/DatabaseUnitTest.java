f1792eaf97dbe1d15594529468444573dc6d1469
Chryfi <71967555+Chryfi@users.noreply.github.com>
2024-01-07 18:46:16 +0100

    Adapt database unit test

--- app/src/androidTest/java/com/chryfi/jogjoy/DatabaseUnitTest.java
+++ app/src/androidTest/java/com/chryfi/jogjoy/DatabaseUnitTest.java
@@ -18,6 +18,10 @@ import com.chryfi.jogjoy.data.tables.GPSTable;
 import com.chryfi.jogjoy.data.tables.RunTable;
 import com.chryfi.jogjoy.data.tables.UserTable;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -33,61 +37,70 @@ public class DatabaseUnitTest {
     public void testDatabase() {
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
         String username = "testuser";
+        List<Run> runs = new ArrayList<>();
         User user = new User(username, "password", 80, 180, User.Gender.MALE);
-        Run run = new Run(12.5F, username);
+        Run run0 = new Run(12.5F, username);
+        runs.add(run0);
+        Run run1 = new Run(3F, username);
+        runs.add(run1);
+        Run run2 = new Run(4F, username);
+        runs.add(run2);
+        Run run3 = new Run(8F, username);
+        runs.add(run3);
 
         try (UserTable userTable = new UserTable(context);
              RunTable runTable = new RunTable(context);
              GPSTable gpsTable = new GPSTable(context)) {
-
             System.out.println("Testing insertion of user.");
             assertTrue(userTable.insertUser(user));
             assertTrue(userTable.getUserByUsername(username).isPresent());
-
-            System.out.println("Testing equality of db gps point and local object.");
             assertEquals(userTable.getUserByUsername(username).get(), user);
 
             System.out.println("Testing insertion of run.");
-            assertTrue(runTable.insertRun(run));
-            assertTrue(runTable.getRunById(run.getId()).isPresent());
-
-            System.out.println("Testing equality of run and local object");
-            assertEquals(runTable.getRunById(run.getId()).get(), run);
-
-            run.addGPSpoint(new GPSPoint(run.getId(), 3, 1.2318458, 2.2318484));
-            run.addGPSpoint(new GPSPoint(run.getId(), 4, 3.15284, 4.842384));
-            run.addGPSpoint(new GPSPoint(run.getId(), 1, 5.16982, 6.18659023));
-            run.addGPSpoint(new GPSPoint(run.getId(), 2, 7.186523, 8.1986503));
-            run.addGPSpoint(new GPSPoint(run.getId(), 2, 1, 2));
-
-            for (GPSPoint point : run.getGpspoints()) {
-                System.out.println("Testing insertion of gps point.");
-                assertTrue(gpsTable.insertGPSPoint(point));
-                assertTrue(gpsTable.getGPSPoint(point.getRunid(), point.getTimestamp()).isPresent());
-
-                System.out.println("Testing equality of db gps point and local object.");
-                assertEquals(gpsTable.getGPSPoint(point.getRunid(), point.getTimestamp()).get(), point);
+            for (Run run : runs) {
+                assertTrue(runTable.insertRun(run));
+                assertTrue(runTable.getRunById(run.getId()).isPresent());
+                assertEquals(runTable.getRunById(run.getId()).get(), run);
+                this.addRandomGPSPoints(run, (int) (Math.random() * 50));
+                this.insertGpsPoints(gpsTable, run);
+                assertEquals(gpsTable.getGPSPoints(run.getId()), run.getGpspoints());
             }
 
-            System.out.println("Testing equality of run and local object");
-            assertEquals(runTable.getRunById(run.getId()).get(), run);
+            runs.sort(Comparator.comparingLong(element -> -element.getGpspoints().get(0).getTimestamp()));
+            assertEquals(runTable.getRunsDescTime(user.getUsername()), runs);
 
             /* deletion */
-            for (GPSPoint point : run.getGpspoints()) {
+            for (Run run : runs) {
                 System.out.println("Testing deletion of gps point.");
-                assertTrue(gpsTable.deleteGPSPoint(point));
-                assertFalse(gpsTable.getGPSPoint(point.getRunid(), point.getTimestamp()).isPresent());
-            }
+                for (GPSPoint point : run.getGpspoints()) {
+                    assertTrue(gpsTable.deleteGPSPoint(point));
+                    assertFalse(gpsTable.getGPSPoint(point.getRunid(), point.getTimestamp()).isPresent());
+                }
 
-            System.out.println("Testing deletion of run.");
-            assertTrue(runTable.deleteRun(run));
-            assertFalse(runTable.getRunById(run.getId()).isPresent());
+                System.out.println("Testing deletion of run.");
+                assertTrue(runTable.deleteRun(run));
+                assertFalse(runTable.getRunById(run.getId()).isPresent());
+            }
 
             System.out.println("Testing deletion of user.");
             assertTrue(userTable.deleteUser(user));
             assertFalse(userTable.getUserByUsername(username).isPresent());
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void insertGpsPoints(GPSTable gpsTable, Run run) {
+        for (GPSPoint point : run.getGpspoints()) {
+            assertTrue(gpsTable.insertGPSPoint(point));
+            assertTrue(gpsTable.getGPSPoint(point.getRunid(), point.getTimestamp()).isPresent());
+            assertEquals(gpsTable.getGPSPoint(point.getRunid(), point.getTimestamp()).get(), point);
+        }
+    }
+
+    private void addRandomGPSPoints(Run run, int amount) {
+        for (int i = 0; i < amount; i++) {
+            run.addGPSpoint(new GPSPoint(run.getId(), (long) (Math.random() * 1000000), Math.random() * 10, Math.random() * 10));
         }
     }
 }
